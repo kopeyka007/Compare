e3669b1aff889aeabf8cc27668c918f98007d698
Taras Kulyk <tk@div-art.com>
2017-03-13 12:32:24 +0200

    add routes laravel

--- routes/web.php
+++ routes/web.php
@@ -10,26 +10,28 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-<<<<<<< .merge_file_a96104
 use Illuminate\Http\Request;
 //use Illuminate\Support\Facades\Blade;
 
 //Blade::setEscapedContentTags('[[', ']]');
 //Blade::setContentTags('[[[', ']]]');
 
-Route::get('/', function () {    
-    return view('welcome');
-=======
+
 Route::get('/', function () {
     return view('template');
->>>>>>> .merge_file_a93316
+});
+Route::get('/compare', function () {
+    return view('template');
+});
+Route::get('/compare/{list}', function () {
+    return view('template');
 });
 
+
 // Маршруты аутентификации...
-Route::get('panel/login', 'Auth\AuthController@getLogin');
-Route::post('panel/login', 'Auth\AuthController@postLogin');
-Route::get('panel/logout', 'Auth\AuthController@getLogout');
+//Route::get('panel/login', 'Auth\AuthController@getLogin');
+//Route::post('panel/login', 'Auth\AuthController@postLogin');
+//Route::get('panel/logout', 'Auth\AuthController@getLogout');
 
 // Маршруты регистрации...
 //Route::get('auth/register', 'Auth\AuthController@getRegister');
