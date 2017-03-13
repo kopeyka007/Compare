6b4d901d2118a45fc30f1bf14922b8f90224fa1a
JulianBeletskyy <redlineonemexico@gmail.com>
2017-03-13 14:50:24 +0200

    Fix bootstrap

--- resources/views/panel/signin.blade.php
+++ resources/views/panel/signin.blade.php
@@ -1,27 +1,26 @@
-<div class="page-signin" ng-controller="signinCtrl">
+<div ng-controller="signinCtrl">
 	<div class="signin-header">
 		<a><img src="#" alt="#"></a>
 	</div>
 
-	<div class="container">
-		<div class="row">
-			<div class="col-md-6">
-				<div class="panel panel-default">
-					<div class="form-container">
-						<form name="form">
-							<div class="form-group">
-								<label>Email address</label>
-								<input type="email" class="form-control" placeholder="Email" name="email" ng-model="email" />
-							</div>
-							<div class="form-group">
-								<label>Password</label>
-								<input type="password" class="form-control" placeholder="Password" name="password" ng-model="password" />
-							</div>
-							<div class="form-group">
-								<button type="submit" class="btn btn-success btn-block text-center" ng-click="signin()">Log in</button>
-							</div>
-						</form>
-					</div>
+
+	<div class="row">
+		<div class="col-md-6">
+			<div class="panel panel-default">
+				<div class="form-container">
+					<form name="form">
+						<div class="form-group">
+							<label>Email address</label>
+							<input type="email" class="form-control" placeholder="Email" name="email" ng-model="email" />
+						</div>
+						<div class="form-group">
+							<label>Password</label>
+							<input type="password" class="form-control" placeholder="Password" name="password" ng-model="password" />
+						</div>
+						<div class="form-group">
+							<button type="submit" class="btn btn-success btn-block text-center" ng-click="signin()">Log in</button>
+						</div>
+					</form>
 				</div>
 			</div>
 		</div>
