d48538ae4c185e3a0ff1c784da96e2b906416d4c
JulianBeletskyy <redlineonemexico@gmail.com>
2017-03-13 12:19:56 +0200

    Angular connect

---
+++ public/js/myapp.js
@@ -0,0 +1,43 @@
+var compare = angular.module('compare', []);
+
+compare.controller('compareController', function() {
+	
+});
+
+compare.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+	$locationProvider.html5Mode(true);
+	var routes, setRoutes;
+
+	routes = [
+		'compare/:alias/',
+		'compare'
+	];
+
+	setRoutes = function(route) {
+		var config, url;
+		url = '/' + route;
+		config = {
+			templateUrl: function(params)
+			{
+				if (params.alias)
+				{
+					return '/pages/' + params.alias;
+				}
+				else
+				{
+					return  '/pages/index';
+				}
+			}
+		};
+		
+		$routeProvider.when(url, config);
+		return $routeProvider;
+	};
+
+	routes.forEach(function(route) {
+		return setRoutes(route);
+	});
+
+   $routeProvider
+		.when('/', {templateUrl: '/pages/index'});
+}]);
