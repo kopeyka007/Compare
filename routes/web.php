d79c7f4dfd5a7854af8629ffd7755ebd4bf20704
Taras Kulyk <tk@div-art.com>
2017-03-14 17:33:58 +0200

    add route list users

--- routes/web.php
+++ routes/web.php
@@ -63,6 +63,7 @@ Route::post('api/signin','AuthController@signin');
 Route::post('api/signout','AuthController@signout');
 Route::get('api/users/info','AuthController@info');
 Route::get('api/users/types','UsersController@get_users_types');
+Route::get('api/users/list','UsersController@get_all');
 Route::post('api/users/save','UsersController@save');
 
 Route::get('users/test','UsersController@test');
