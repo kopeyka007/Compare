6ac680ac0be8434b4f5fcb09babc4b19fce206b4
Ievgenii Domanskyi <id@div-art.com>
2017-03-20 15:41:44 +0200

    Compare

--- public/js/compare.js
+++ public/js/compare.js
@@ -1,22 +1,20 @@
 (function() {
 	angular.module('compareApp').controller('compareCtrl', ['$scope', '$rootScope', '$http', '$window', '$location', compareCtrl]);
 	function compareCtrl($scope, $rootScope, $http, $window, $location) {
-		
+		$scope.mode = 'all';
 		$scope.filterList = [];
 		$scope.compareList = [];
 		var url = $location.path();
 		
 		$http.post('/api/compare/list', {url}).then(function(response){
 			$scope.compareList = response.data.data;
-			console.log($scope.compareList);
 		});
 		
 		$http.post('/api/compare/catsfilters', {url}).then(function(response){
 			$scope.filterList = response.data.data;
-			
 		});
 		
-		$scope.closeLink = function(prods_id, alias) {
+		$scope.closeLink = function(prods_id) {
 			var aliases = [];
 			for (var id in $scope.compareList)
 			{
@@ -27,9 +25,34 @@
 					$scope.selectedCount++;
 				}
 			}
-			$scope.preLink = '/compare/';
-			return $scope.preLink + aliases.join('-vs-');
-			
+			return '/compare/' + aliases.join('-vs-');
+		};
+
+		$scope.checkDifferences = function(value, index, array) {
+			if ($scope.mode == 'diff')
+			{
+				var check = false;
+				var start = '';
+				for (var id in $scope.compareList)
+				{
+					var prod = $scope.compareList[id];
+					if (start == '')
+					{
+						start = prod.filters[value.filters_id];
+					}
+
+					if (start != prod.filters[value.filters_id])
+					{
+						check = true;
+					}
+				}
+
+				return check;
+			}
+			else
+			{
+				return true;
+			}
 		};
 	}
 })();
--- resources/views/compare/compare.blade.php
+++ resources/views/compare/compare.blade.php
@@ -1,64 +1,65 @@
 <div ng-controller="compareCtrl">
 	<div class="container">
 		<div class="table-responsive">
-			<table class="table table-striped compare-table">
+			<table class="table compare-table">
 				<thead>
 					<tr>
-						<th></th>
-						<th ng-repeat="i in [0, 1, 2, 3]">
-							<h4 ng-if="compareList[i]">@{{compareList[i].brands_id.brands_name}} @{{compareList[i].prods_name}}</h4>
+						<th class="filters-cell"></th>
+						<th class="prods-cell" ng-repeat="i in [0, 1, 2, 3]">
+							<h4 ng-if="compareList[i]">@{{compareList[i].prods_name}}</h4>
+							<p>@{{compareList[i].brands_id.brands_name}}</p>
 						</th>
 					</tr>
 				</thead>
+
 				<tbody>
 					<tr>
-						<td>
-							<h3>Show:</h3>
-							<div>
-								<label class="btn btn-success btn-block">
-									<input type="checkbox" />
-									<span>All Features</span>
-								</label>
-							</div>
-							<div>
-								<label class="btn btn-success btn-block">
-									<input type="checkbox" />
-									<span>Differences</span>
-								</label>
-							</div>
+						<td class="filters-cell">
+							<button type="button" class="btn btn-success btn-block" ng-class="{'active': mode == 'all'}" ng-click="mode = 'all'">
+								All Features
+							</button>
+
+							<button type="button" class="btn btn-success btn-block" ng-class="{'active': mode == 'diff'}" ng-click="mode = 'diff'">
+								Differences
+							</button>
 						</td>
-						<td class="td-header" ng-repeat="i in [0, 1, 2, 3]">
+
+						<td class="prods-cell" ng-repeat="i in [0, 1, 2, 3]">
 							<div class="compare-head" ng-if="compareList[i]" ng-init="prod = compareList[i]">
-								<div class="compare-img text-center">
+								<a href="/@{{ prod.cats_id }}/" class="compare-link">
 									<img src="@{{prod.prods_foto}}" alt="#" />
-								</div>
-								<div class="compare-price text-danger">
-									$@{{prod.prods_price}}
-								</div>
+
+									<span class="compare-price text-danger">
+										$@{{prod.prods_price}}
+									</span>
+								</a>
+
 								<a href="@{{closeLink(prod.prods_id)}}" class="compare-close">
 									<i class="fa fa-times-circle" aria-hidden="true"></i>
 								</a>
+
 								<div class="wrap-add-btn">
 									<button class="btn btn-info add-btn">Add Another Products</button>
 								</div>
 							</div>
+
 							<div ng-if="! compareList[i]" class="compare-head inactive">
-								<div class="compare-img text-center">
+								<div class="compare-link">
 									<img src="http://comparewear.com/images/products/apple-watch.jpg" alt="#" />
+
+									<span class="compare-price text-danger">
+										$9.999
+									</span>
 								</div>
-								<div class="compare-price text-danger">
-									$9.999
-								</div>
-								<div class="compare-close">
-									<i class="fa fa-times-circle" aria-hidden="true" ng-click=""></i>
-								</div>
+
 								<div class="wrap-add-btn">
 									<button class="btn btn-info add-btn">Add Another Products</button>
 								</div>
 							</div>
 						</td>
 					</tr>
-					<tr>
+
+					<!--tr>
 						<td><h3>Overview</h3></td>
 						<td colspan="4"><h3>Advantages (Factors To Decide Which Device You Should Buy)</h3></td>
 					</tr>
@@ -90,21 +91,21 @@
 						</td>
 						<td></td>
 						<td></td>
-					</tr>
+					</tr-->
 				</tbody>
 			</table>
 			
 			<div class="groups-box" ng-repeat="group in filterList">
 				<h3>@{{group.groups_name}}</h3>
-				<table class="table">
+				<table class="table table-striped">
 					<tbody>
-						<tr ng-repeat="filter in group.groups_filters">
-							<td class="td-name">@{{filter.filters_name}}</td>
-							<td class="td-header" ng-repeat="i in [0, 1, 2, 3]">
-								<div class="compare-head" ng-if="compareList[i]" ng-init="prod = compareList[i]">
-									<div class="compare-price text-danger">
-										@{{prod.filters[filter.filters_id].filters_value}}
-									</div>
+						<tr ng-repeat="filter in group.groups_filters | filter:checkDifferences">
+							<td class="filters-cell">@{{filter.filters_name}}</td>
+							<td class="prods-cell" ng-repeat="i in [0, 1, 2, 3]">
+								<div ng-if="compareList[i]" ng-init="prod = compareList[i]">
+									<i class="fa fa-check-circle text-success" ng-show="filter.filters_type == 'check' && prod.filters[filter.filters_id].filters_value"></i>
+									<i class="fa fa-times-circle text-danger" ng-show="filter.filters_type == 'check' && ! prod.filters[filter.filters_id].filters_value"></i>
+									<span ng-show="filter.filters_type != 'check'">@{{prod.filters[filter.filters_id].filters_value}}</span>
 								</div>
 							</td>
 						</tr>
