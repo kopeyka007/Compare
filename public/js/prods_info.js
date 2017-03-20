222dc7b34bad93ee95ca546d3a7cfbd9d2815990
JulianBeletskyy <redlineonemexico@gmail.com>
2017-03-20 12:56:31 +0200

    commit

--- public/js/prods_info.js
+++ public/js/prods_info.js
@@ -2,11 +2,11 @@
 	angular.module('compareApp').controller('prodsInfoCtrl', ['$scope', '$rootScope', '$http', '$window', '$location', prodsInfoCtrl]);
 	function prodsInfoCtrl($scope, $rootScope, $http, $window, $location) {
 
-		var url = $location.path();
+	/* 	var url = $location.path();
 		
 		$http.post('/api/compare/list', {url}).then(function(response){
 			$scope.prodsInfo = response.data.data;
 			console.log($scope.prodsInfo);
-		});
+		}); */
 	}
 })();
