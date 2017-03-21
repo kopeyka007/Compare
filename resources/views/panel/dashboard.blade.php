99480e5cc74e3b34cace8348c477c00aed02bb6d
JulianBeletskyy <redlineonemexico@gmail.com>
2017-03-21 18:07:34 +0200

    commit

--- resources/views/panel/dashboard.blade.php
+++ resources/views/panel/dashboard.blade.php
@@ -14,9 +14,17 @@
 				<div class="panel panel-default">
 					<div class="panel-heading">Top 10 Most Compared products (Single)</div>
 					<div class="panel-body">
-						<ol>
-							<li ng-repeat="singleTop in statDashboard.single_compare_top10">@{{singleTop.prods.prods_name + ' ' + singleTop.prods_count}}</li>
-						</ol>
+						<div class="table-responsive">
+							<table class="table table-striped">
+								<tbody>
+									<tr ng-repeat="(i, singleTop) in statDashboard.single_compare_top10">
+										<td class="td-number">@{{i + 1}}.</td>
+										<td>@{{singleTop.prods.brands_id.brands_name}} @{{singleTop.prods.prods_name}}</td>
+										<td class="td-counter">@{{singleTop.prods_count}}</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
 					</div>
 				</div>
 			</div>
@@ -24,9 +32,17 @@
 				<div class="panel panel-default">
 					<div class="panel-heading">Top 10 Most Compared products (Pair)</div>
 					<div class="panel-body">
-						<ol>
-							<li></li>
-						</ol>
+						<div class="table-responsive">
+							<table class="table table-striped">
+								<tbody>
+									<tr ng-repeat="(i, singleTop) in statDashboard.single_compare_top10">
+										<td class="td-number">@{{i + 1}}.</td>
+										<td>@{{singleTop.prods.brands_id.brands_name}} @{{singleTop.prods.prods_name}}</td>
+										<td class="td-counter">@{{singleTop.prods_count}}</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
 					</div>
 				</div>
 			</div>
@@ -42,9 +58,17 @@
 				<div class="panel panel-default">
 					<div class="panel-heading">Top 10 Products with Most Amazon Clicks</div>
 					<div class="panel-body">
-						<ol>
-							<li ng-repeat="amazonTop in statDashboard.amazon_top10">@{{amazonTop.prods.prods_name + ' ' + amazonTop.prods_count}}</li>
-						</ol>
+						<div class="table-responsive">
+							<table class="table table-striped">
+								<tbody>
+									<tr ng-repeat="(i, singleTop) in statDashboard.single_compare_top10">
+										<td class="td-number">@{{i + 1}}.</td>
+										<td>@{{singleTop.prods.brands_id.brands_name}} @{{singleTop.prods.prods_name}}</td>
+										<td class="td-counter">@{{singleTop.prods_count}}</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
 					</div>
 				</div>
 			</div>
