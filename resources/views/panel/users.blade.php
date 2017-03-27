0ba9073cb0604dcbc983ea7ef2a538728932821b
JulianBeletskyy <redlineonemexico@gmail.com>
2017-03-27 12:53:21 +0300

    Fix reqest

--- resources/views/panel/users.blade.php
+++ resources/views/panel/users.blade.php
@@ -71,7 +71,7 @@
 			
 			<div class="col-sm-12" ng-show="user.type.id == 2 || user.type.id == 3">
 				<div class="user-cats-box" ng-repeat="cat in cats">
-					<input type="checkbox" ng-model="user[cat.cats_id].cats.cats_id" />
+					<input type="checkbox" ng-model="user.cats[cat.cats_id]" />
 					<label>@{{cat.cats_name}}</label>
 				</div>
 			</div>
