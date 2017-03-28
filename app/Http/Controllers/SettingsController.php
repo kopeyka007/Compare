7fa2bccd5c4275423764c40ed397a59b47cc8628
Taras Kulyk <tk@div-art.com>
2017-03-28 17:42:36 +0200

    add migration, seed and controller for settings

---
+++ app/Http/Controllers/SettingsController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Settings;
+use Illuminate\Http\Request;
+use Config;
+
+class SettingsController extends Controller
+{
+  public function __construct()
+  {
+    
+  }
+
+  public function show(){       
+    return view('panel.settings');
+  }
+
+  public function get_all(){    
+    $settings = Settings::first();
+    $response['data'] = $settings;
+    return $response;    
+  }
+  
+  public function save(Request $request){    
+    $current = Settings::find(1);
+    if ($current){
+      $current->s3_key = $request->input('s3_key');        
+      $current->s3_secret = $request->input('s3_secret');                   
+      $current->s3_region = $request->input('s3_region');      
+      $current->s3_bucket = $request->input('s3_bucket');                
+      if ($current->save()){
+        $response['data'] = true;          
+        $response['message'] = ['type'=>'success', 'text'=>'Settings saved'];
+      }
+    }
+    return $response;
+  }
+  
+  public static function set_config_s3(){
+    $current = Settings::find(1);    
+    Config::set('filesystems.disks.s3.key', $current->s3key);
+    Config::set('filesystems.disks.s3.secret', $current->s3_secret);
+    Config::set('filesystems.disks.s3.region', $current->s3_region);
+    Config::set('filesystems.disks.s3.bucket', $current->s3_bucket);
+  }
+  
+}
---
+++ database/seeds/SettingsTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SettingsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('settings')->insert([
+            's3_key' => '',
+            's3_secret' => '',
+            's3_region' => '',
+            's3_bucket' => '',
+        ]);
+    }
+}
